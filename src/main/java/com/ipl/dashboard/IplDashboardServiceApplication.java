cabebecbed7a80c338ca152206c1e03da5221731
ramji013 <ramprm@gmail.com>
2021-04-20 04:49:32 +0800

    initial check-in

---
+++ src/main/java/com/ipl/dashboard/IplDashboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ipl.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashboardServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ipl/dashboard/model/Match.java
@@ -0,0 +1,23 @@
+package com.ipl.dashboard.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Match {
+    private long id;
+    private String city;
+    private LocalDate date;
+    private String playerOfmatch;
+    private String venue;
+    private String team1;
+    private String team2;
+    private String tossWinner;
+    private String tossDecision;
+    private String matchWinner;
+    private String result;
+    private String resultMargin;
+    private String umpire1;
+    private String umpire2;
+}
