8352faee3b131e5529bd262a9dd77af230e147b8
ramji013 <ramprm@gmail.com>
2021-04-26 05:12:46 +0800

    created team table and data populated

--- src/main/java/com/ipl/dashboard/model/Team.java
+++ src/main/java/com/ipl/dashboard/model/Team.java
@@ -1,2 +1,35 @@
-package com.ipl.dashboard.model;public class Team {
+package com.ipl.dashboard.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public Team(String teamName, long id){
+        this.teamName = teamName;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "teamName='" + teamName + '\'' +
+                ", totalMatches=" + totalMatches +
+                ", totalWins=" + totalWins +
+                '}';
+    }
 }
--- src/main/java/com/ipl/dashboard/processor/JobCompletionNotificationListener.java
+++ src/main/java/com/ipl/dashboard/processor/JobCompletionNotificationListener.java
@@ -1,5 +1,6 @@
 package com.ipl.dashboard.processor;
 
+import com.ipl.dashboard.model.Team;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
@@ -9,24 +10,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
     private static final Logger logger = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
-    private final JdbcTemplate jdbcTemplate;
+    private final EntityManager em;
 
     @Autowired
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate = jdbcTemplate;
+    public JobCompletionNotificationListener(EntityManager em){
+        this.em = em;
     }
 
     @Override
+   @Transactional
     public void afterJob(JobExecution jobExecution){
-        if(jobExecution.getStatus()== BatchStatus.COMPLETED){
-            logger.info("!!! JOB FINISHED ! Time to verify the results.");
-            jdbcTemplate.query("select team1, team2, date from match",
-                    (rs, row) -> "Team 1 " + rs.getString(1) + " Team 2 " + rs.getString(2) + " Date : " + rs.getString(3))
-            .forEach(str -> System.out.println(str));
+        if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            logger.info("!!! JOB FINISHED! Time to verify the results");
+
+            Map<String, Team> teamData = new HashMap<>();
+
+            em.createQuery("select m.team1, count(*) from Match m group by m.team1", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .map(e -> new Team((String) e[0], (long) e[1]))
+                    .forEach(team -> teamData.put(team.getTeamName(), team));
+
+            em.createQuery("select m.team2, count(*) from Match m group by m.team2", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .forEach(e -> {
+                        Team team = teamData.get((String) e[0]);
+                        team.setTotalMatches(team.getTotalMatches() + (long) e[1]);
+                    });
+
+            em.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .forEach(e -> {
+                        Team team = teamData.get((String) e[0]);
+                        if (team != null) team.setTotalWins((long) e[1]);
+                    });
+
+            teamData.values().forEach(team -> em.merge(team));
+            teamData.values().forEach(team -> System.out.println(team));
         }
 
     }
